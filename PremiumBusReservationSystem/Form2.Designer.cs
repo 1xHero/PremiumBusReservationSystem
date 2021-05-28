2b52df3e750075d5ed2bbd9ddd19c5646d2b759c
thunderbird01 <32418945+thunderbird01@users.noreply.github.com>
2021-05-29 00:55:28 +0300

    v1.5.2

--- PremiumBusReservationSystem/Form2.Designer.cs
+++ PremiumBusReservationSystem/Form2.Designer.cs
@@ -33,7 +33,6 @@ namespace PremiumBusReservationSystem
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -70,16 +69,6 @@ namespace PremiumBusReservationSystem
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(330, 71);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(132, 40);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // button4
             // 
             this.button4.Location = new System.Drawing.Point(330, 201);
@@ -96,7 +85,6 @@ namespace PremiumBusReservationSystem
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(489, 263);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
@@ -114,7 +102,6 @@ namespace PremiumBusReservationSystem
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
     }
 }
